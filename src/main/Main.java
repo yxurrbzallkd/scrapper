0bac01413c5375a8d6f292456a8591fc69b24f9e
yxurrbzallkd <kypybida@ucu.edu.ua>
2020-12-15 01:52:55 +0200

    second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,4 @@
+import org.jsoup.nodes.Document;
 import scraper.*;
 import org.jsoup.Jsoup;
 
@@ -8,17 +9,31 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
-        //Scraper s = new URLScraper();
-
         Scraper s = new CacheScraper(new URLScraper());
-        String db = "jdbc:sqlite:"+System.getProperty("user.dir")+"\\cached.db";
+        String scrape = s.scrape("https://keepcodeclean.com/web-scraping-using-java-jsoup/");
+        //System.out.println(scrape);
+        scrape = s.scrape("https://keepcodeclean.com/web-scraping-using-java-jsoup/");
+        //System.out.println(scrape);
+        /*
+        String db = "jdbc:sqlite:"+System.getProperty("user.dir")+"\\src\\main\\scraper\\cached.db";
+        System.out.println(db);
         Connection connection = DriverManager.getConnection(db);
         Statement statement = connection.createStatement();
-        String url = "https://www.wikipedia.org/";
-        String pageText = s.scrape(url);
-        statement.executeUpdate("insert into table values ("+url+", "+pageText+")");
+        statement.executeUpdate("drop table if exists cached");
+        statement.executeUpdate("create table if not exists cached (url string, page text)");
+        String url = "https://keepcodeclean.com/web-scraping-using-java-jsoup/";
+        Document doc = Jsoup.connect(url).get();
+        String page = doc.toString();
+        statement.executeUpdate("insert into cached(url) values ('"+url+"')");
+        PreparedStatement pstmt =
+                connection.prepareStatement("insert into cached(page) values (?)");
+        pstmt.setString(1, page);
+        System.out.println("insert into cached(page) values ('"+page+"')");
+        pstmt.executeUpdate();
+        pstmt.close();*/
     }
 }
--- src/main/scraper/CacheScraper.java
+++ src/main/scraper/CacheScraper.java
@@ -2,11 +2,7 @@ package scraper;
 
 import java.io.IOException;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class CacheScraper implements Scraper {
     private Scraper scraper;
@@ -16,28 +12,46 @@ public class CacheScraper implements Scraper {
     public CacheScraper(Scraper scraper) throws SQLException{
         this.scraper = scraper;
         //open database
-        String db = "jdbc:sqlite:"+System.getProperty("user.dir")+"\\cached.db";
+        String db = "jdbc:sqlite:"+System.getProperty("user.dir")+"\\src\\main\\scraper\\cached.db";
+        System.out.println(db);
         connection = DriverManager.getConnection(db);
-        System.out.println("successfully connected to database");
-        this.statement = connection.createStatement();
+        statement = connection.createStatement();
+        statement.executeUpdate("drop table if exists cached");
+        statement.executeUpdate("create table if not exists cached (url string, page string)");
     }
 
     public String scrape(String url) throws SQLException, IOException {
         String pageText = checkPageInCache(url);
-        if (pageText.length() != 0) {return pageText;}
+        if (pageText.length() != 0) {
+            System.out.println("returning cached "+url);
+            return pageText;
+        }
         pageText = scraper.scrape(url);
         storeInCache(url, pageText);
         return pageText;
     }
 
     public String checkPageInCache(String url) throws SQLException {
-        ResultSet rs = statement.executeQuery("select * from url where url='"+url+"'");
-        int id = rs.getInt("id");
+        ResultSet rs = statement.executeQuery("select url, page from cached where url='"+url+"'");
+        while (rs.next()) {
+            String pageText = rs.getString("page");
+            System.out.println(pageText);
+            if (pageText != null) {
+                return pageText;
+            }
+        }
         return "";
     }
 
     public void storeInCache(String url, String pageText) throws SQLException {
+        System.out.println("storing "+url+" in cache");
         //implement storing in cache
-        statement.executeUpdate("insert into table values ("+url+", "+pageText+")");
+        statement.executeUpdate("insert into cached(url) values ('"+url+"')");
+        PreparedStatement pstmt =
+                connection.prepareStatement("insert into cached(page) values (?)");
+        pstmt.setString(1, pageText);
+        statement.executeUpdate("insert into cached(url) values ('"+url+"')");
+        pstmt.executeUpdate();
+        pstmt.close();
     }
 }
